24c62342abafe57b709026cf3673de0f524953fa
Gustavo Lozada <gustavo.lozada@udea.edu.co>
2025-04-26 11:46:37 -0500

    proxy and decorator pattern glozada@bancolombia.com.co Gustavo Lozada

---
+++ src/main/java/com/example/patterns_banking/models/decorator/WithDrawDecorator.java
@@ -0,0 +1,2 @@
+package com.example.patterns_banking.models.decorator;public class WithDrawDecorator {
+}
---
+++ src/main/java/com/example/patterns_banking/services/proxy/ICustomerOperations.java
@@ -0,0 +1,2 @@
+package com.example.patterns_banking.services.proxy;public interface ICustomerOperations {
+}
